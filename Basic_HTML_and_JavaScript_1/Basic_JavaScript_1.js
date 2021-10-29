13f99b674b981221e89ea5677dbac82fd7fc163b
techwolfd <93291605+techwolfd@users.noreply.github.com>
2021-10-28 18:09:49 -0700

    Update Basic_JavaScript_1.js

--- Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -1,1 +1,18 @@
-alert("Hi, W, Alert of box")
+alert("Hi, W, Alert of box");
+
+var student ={
+    firstname :"sw",
+    lastname:"SW",
+    age :28,
+    gradeaverage: 3.5
+    
+};
+
+function add(n1, n2){
+    return n1 + n2;
+}
+var sum + add(5,7);
+
+<h1 id="header_1"> this is my heading</h1>
+
+document.getElementById("header_1").innerHTML
